1a06c03a484b086ac0809b640eb86b31490cbbed
Tobias Lengsholz <t.lengsholz@neusta.de>
2016-11-07 10:14:26 +0100

    added functionality to prepend an html-import of the created style module

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var through = require('through2');
 var path = require('path');
 var gutil = require('gulp-util');
 var PLUGIN_NAME = 'gulp-style-modules';
+var prependFile = require('prepend-file');
+var fs = require('fs');
 
 module.exports = function(opts) {
     function namefn(file) {
@@ -13,6 +15,7 @@ module.exports = function(opts) {
 
     var fname = opts && opts.filename || namefn;
     var modid = opts && opts.moduleId || namefn;
+    var iFile = opts && opts.includeFile;
 
     return through.obj(function (file, enc, cb) {
 
@@ -25,21 +28,39 @@ module.exports = function(opts) {
 
         var filename = typeof fname === 'function' ? fname(file) : fname;
         var moduleId = typeof modid === 'function' ? modid(file) : modid;
+        var includeFile = typeof iFile === 'function' ? iFile(file) : iFile;
+
         var dirname = path.dirname(file.path);
+        var importStyleModule = '<link rel="import" href="' + filename + '.html' + '">';
 
         var res = '<dom-module id="' + moduleId + '">\n' +
-            '<template>\n' +
-            '<style>\n' +
-            file.contents.toString('utf8') + '\n' +
-            '</style>\n' +
-            '</template>\n' +
-            '</dom-module>';
+          '<template>\n' +
+          '<style>\n' +
+          file.contents.toString('utf8') + '\n' +
+          '</style>\n' +
+          '</template>\n' +
+          '</dom-module>';
 
         file.contents = new Buffer(res);
         file.path = path.join(dirname, filename) + '.html';
+
+        //make sure includeFile argument actually points to an existing file
+        if(includeFile) {
+            fs.exists(includeFile, function(exists) {
+                if (exists) {
+                    //put include into original component
+                    prependFile(includeFile, importStyleModule, function(err) {
+                        if (err) {
+                            // Error
+                            return cb(new gutil.PluginError(PLUGIN_NAME, err));
+                        }
+                        // Success
+                    });
+                }
+            });
+        }
 
         return cb(null, file);
     });
 
 };
-
